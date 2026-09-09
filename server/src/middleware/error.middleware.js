3b34ba6583572bebacfcde5f1d042ee01ac781cb
abhishekboken12 <abhishekboken12@gmail.com>
2026-09-09 07:11:56 +0530

    stated express server

--- server/src/middleware/error.middleware.js
+++ server/src/middleware/error.middleware.js
@@ -1,4 +1,4 @@
-xport const notFoundHandler = (req, res, next) => {
+export const notFoundHandler = (req, res, next) => {
   res.status(404).json({
     success: false,
     message: `Route not found: ${req.method} ${req.originalUrl}`,
